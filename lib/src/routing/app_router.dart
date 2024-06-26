bae2ac36c494ea1d5c8de39b4bd7c7366ab8daa9
caiombueno <caiomeirabueno@hotmail.com>
2024-06-26 15:14:44 -0300

    feat: set up GoRouter for navigation

---
+++ lib/src/routing/app_router.dart
@@ -0,0 +1,18 @@
+import 'package:coolmovies/src/features/movie_list/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+part 'app_router.g.dart';
+
+final router = GoRouter(
+  initialLocation: const HomeRoute().location,
+  debugLogDiagnostics: true,
+  routes: $appRoutes,
+);
+
+@TypedGoRoute<HomeRoute>(path: '/')
+class HomeRoute extends GoRouteData {
+  const HomeRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) => const HomeScreen();
+}
