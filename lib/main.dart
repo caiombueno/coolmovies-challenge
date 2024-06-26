84c3318a7218aaee9e2e0f3e6665f14ef4ce1ef1
caiombueno <caiomeirabueno@hotmail.com>
2024-06-26 16:05:16 -0300

    feat: add global error handlers and UI for uncaught exceptions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:coolmovies/src/app/app.dart';
+import 'package:coolmovies/src/app/app_bloc_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() async {
@@ -21,8 +25,36 @@ void main() async {
   );
 
   Bloc.observer = AppBlocObserver();
+
+  // https://docs.flutter.dev/testing/errors
+  registerErrorHandlers();
+
   runApp(GraphQLProvider(
     client: client,
     child: const App(),
   ));
+}
+
+void registerErrorHandlers() {
+  // Show some error UI if any uncaught exception happens
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+  // Handle errors from the underlying platform/OS
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+    log(error.toString(), stackTrace: stack, error: error);
+    return true;
+  };
+  // Show some error UI when any widget in the app fails to build
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text('An error occurred'),
+      ),
+      body: Center(child: Text(details.toString())),
+    );
+  };
 }
