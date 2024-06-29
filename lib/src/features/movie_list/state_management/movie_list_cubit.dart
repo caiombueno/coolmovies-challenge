d79c1c84d204e3c4b57730b189c97a3cae769a57
caiombueno <caiomeirabueno@hotmail.com>
2024-06-29 10:02:58 -0300

    feat: add MovieListCubit

---
+++ lib/src/features/movie_list/state_management/movie_list_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:coolmovies/src/data/repositories/movie_repository.dart';
+import 'package:coolmovies/src/models/exceptions.dart';
+import 'package:coolmovies/src/models/movie_summary.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+part 'movie_list_state.dart';
+
+@lazySingleton
+class MovieListCubit extends Cubit<MovieListState> {
+  final MovieRepository _movieRepository;
+
+  MovieListCubit(this._movieRepository) : super(const MovieListLoading());
+
+  Future<void> getMovieSummaryList() async {
+    final movies = await _movieRepository.getMovieSummaryList();
+    emit(
+      movies.fold(
+        (exception) => MovieListFailed(exception),
+        (summaries) => MovieListLoaded(summaries),
+      ),
+    );
+  }
+
+  @disposeMethod
+  @override
+  Future<void> close() => super.close();
+}
--- lib/src/service_location/service_location.config.dart
+++ lib/src/service_location/service_location.config.dart
@@ -9,7 +9,10 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:coolmovies/src/data/data_sources/data_sources.dart' as _i6;
-import 'package:coolmovies/src/data/repositories/repositories.dart' as _i5;
+import 'package:coolmovies/src/data/data_sources/movie_data_source.dart' as _i3;
+import 'package:coolmovies/src/data/repositories/movie_repository.dart' as _i5;
+import 'package:coolmovies/src/features/movie_list/state_management/movie_list_cubit.dart'
+    as _i7;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:graphql/client.dart' as _i4;
 import 'package:injectable/injectable.dart' as _i2;
@@ -25,10 +28,14 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.lazySingleton<_i6.MovieDataSource>(
-        () => _i6.MovieDataSource(gh<_i4.GraphQLClient>()));
+    gh.lazySingleton<_i3.MovieDataSource>(
+        () => _i3.MovieDataSource(gh<_i4.GraphQLClient>()));
     gh.lazySingleton<_i5.MovieRepository>(
         () => _i5.MovieRepository(gh<_i6.MovieDataSource>()));
+    gh.lazySingleton<_i7.MovieListCubit>(
+      () => _i7.MovieListCubit(gh<_i5.MovieRepository>()),
+      dispose: (i) => i.close(),
+    );
     return this;
   }
 }
