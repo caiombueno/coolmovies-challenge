80e383ce5126d153443540e3b298fca545012114
caiombueno <caiomeirabueno@hotmail.com>
2024-06-29 10:03:12 -0300

    test: add tests to MovieListCubit

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,4 +1,5 @@
 import 'package:coolmovies/src/data/data_sources/data_sources.dart';
+import 'package:coolmovies/src/data/repositories/repositories.dart';
 import 'package:graphql/client.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -7,3 +8,5 @@ class MockGraphQLClient extends Mock implements GraphQLClient {}
 class MockQueryResult<T> extends Mock implements QueryResult<T> {}
 
 class MockMovieDataSource extends Mock implements MovieDataSource {}
+
+class MockMovieRepository extends Mock implements MovieRepository {}
---
+++ test/src/features/movie_list/state_management/movie_list_cubit_test.dart
@@ -0,0 +1,71 @@
+import 'package:coolmovies/src/data/repositories/repositories.dart';
+import 'package:coolmovies/src/features/movie_list/state_management/state_management.dart';
+import 'package:coolmovies/src/models/models.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../mocks.dart';
+
+void main() {
+  group('MovieListCubit', () {
+    late MovieRepository movieRepository;
+    late MovieListCubit movieListCubit;
+
+    const movieSummaries = <MovieSummary>[
+      MovieSummary(id: '1'),
+      MovieSummary(id: '2'),
+    ];
+
+    setUp(() {
+      movieRepository = MockMovieRepository();
+      movieListCubit = MovieListCubit(movieRepository);
+    });
+
+    tearDown(() {
+      movieListCubit.close();
+    });
+
+    test('initial state is MovieListLoading',
+        () => expect(movieListCubit.state, const MovieListLoading()));
+
+    group('getMovieSummaryList', () {
+      void verifySingleCallAndNoMoreInteractions() {
+        verify(() => movieRepository.getMovieSummaryList()).called(1);
+        verifyNoMoreInteractions(movieRepository);
+      }
+
+      blocTest<MovieListCubit, MovieListState>(
+          'emits [MovieListLoaded] when getMovieSummaryList succeeds',
+          setUp: () =>
+              when(() => movieRepository.getMovieSummaryList()).thenAnswer(
+                (_) async => const Right(movieSummaries),
+              ),
+          build: () => movieListCubit,
+          act: (cubit) => cubit.getMovieSummaryList(),
+          expect: () => [isA<MovieListLoaded>()],
+          verify: (cubit) {
+            verifySingleCallAndNoMoreInteractions();
+            final movies = (cubit.state as MovieListLoaded).movies;
+            expect(movies, movieSummaries);
+          });
+
+      blocTest<MovieListCubit, MovieListState>(
+        'emits [MovieListFailed] when getMovieSummaryList fails',
+        setUp: () =>
+            when(() => movieRepository.getMovieSummaryList()).thenAnswer(
+          (_) async => const Left(MoviesFetchFailureException()),
+        ),
+        build: () => movieListCubit,
+        act: (cubit) => cubit.getMovieSummaryList(),
+        expect: () => [isA<MovieListFailed>()],
+        verify: (cubit) {
+          verifySingleCallAndNoMoreInteractions();
+          final exception = (cubit.state as MovieListFailed).exception;
+          expect(exception, const MoviesFetchFailureException());
+        },
+      );
+    });
+  });
+}
