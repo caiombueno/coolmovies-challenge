8749db2f41d86dced2e35ffb762f2392f1d95743
caiombueno <caiomeirabueno@hotmail.com>
2024-06-30 17:09:41 -0300

    feat: add MovieDetailsCubit

---
+++ lib/src/features/movie_details/state_management/movie_details_state.dart
@@ -0,0 +1,30 @@
+part of 'movie_details_cubit.dart';
+
+abstract class MovieDetailsState extends Equatable {
+  const MovieDetailsState();
+}
+
+class MovieDetailsLoading extends MovieDetailsState {
+  const MovieDetailsLoading();
+
+  @override
+  List<Object> get props => [];
+}
+
+class MovieDetailsLoaded extends MovieDetailsState {
+  final MovieDetails movieDetails;
+
+  const MovieDetailsLoaded(this.movieDetails);
+
+  @override
+  List<Object> get props => [movieDetails];
+}
+
+class MovieDetailsFailed extends MovieDetailsState {
+  final AppException exception;
+
+  const MovieDetailsFailed(this.exception);
+
+  @override
+  List<Object> get props => [exception];
+}
