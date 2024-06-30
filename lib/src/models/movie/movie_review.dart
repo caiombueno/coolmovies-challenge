8dfb54649bef186d825848d0a9a93f146ec94d99
caiombueno <caiomeirabueno@hotmail.com>
2024-06-30 15:52:33 -0300

    feat: add MovieReview model

---
+++ lib/src/models/movie/movie_review.dart
@@ -0,0 +1,20 @@
+import 'package:coolmovies/src/models/models.dart';
+import 'package:equatable/equatable.dart';
+
+class MovieReview extends Equatable {
+  final String reviewId;
+  final String? title, body;
+  final double? rating;
+  final User? reviewer;
+
+  const MovieReview({
+    required this.reviewId,
+    this.title,
+    this.body,
+    this.reviewer,
+    this.rating,
+  });
+
+  @override
+  List<Object?> get props => [reviewId, title, body, reviewer, rating];
+}
